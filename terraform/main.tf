725043e49f4cf59847c58f07fcc94964565afd1a
Martin Jahr <martin@desixma.org>
2020-11-14 23:40:12 +0100

    cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,8 +142,6 @@ resource "hcloud_server" "postgres" {
   server_type = var.postgres_type
   location    = var.location
   labels      = {
-      "Name"     = var.env_name
-      "Stage"    = var.env_stage
       "POSTGRES" = 0
   }
   ssh_keys    = [ var.keyname ]
@@ -202,8 +200,6 @@ resource "hcloud_server" "worker" {
   server_type = var.worker_type
   location    = var.location
   labels      = {
-      "Name"     = var.env_name
-      "Stage"    = var.env_stage
       "WORKER" = 0
   }
   ssh_keys    = [ var.keyname ]
@@ -262,8 +258,6 @@ resource "hcloud_server" "controller" {
   server_type = var.controller_type
   location    = var.location
   labels      = {
-      "Name"     = var.env_name
-      "Stage"    = var.env_stage
       "CONTROLLER" = 0
   }
   ssh_keys    = [ var.keyname ]
